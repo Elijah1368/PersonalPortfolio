164339219586e7cf37535f531a60ae7af50f0d6c
Elijah Amian <elijah25@uw.edu>
2023-07-30 04:16:48 -0700

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-<a target="_blank" href="https://icons8.com/icon/13027/briefcase">Briefcase</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
-<a target="_blank" href="https://icons8.com/icon/19293/code">Code</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+Personal Portfolio
+======================
+Click on the image to view website
+<a target="_blank" href="https://elijah1368.github.io/PersonalPortfolio/"><img src="./WebsitePhoto.png"/></a>
+
+
+
+Made with React, Typescript, SCSS, and Vite
+
+Inspired by MacOS design
+
+Wallpaper from MacOS ElCapitan
+
+
+<a target="_blank" href="https://icons8.com"> Icons by Icons8</a>
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,12 +1,11 @@
-import classNames from 'classnames';
-import styles from './home.module.scss';
-import { NameAndTitle } from '../name-and-title/name-and-title';
-import { Box, Container } from '@mui/material';
-import { Section } from '../common/section';
-import { WALLPAPER } from '../../constants';
+
+import { NameAndTitle } from '../name-and-title/name-and-title'
+
+import { Section } from '../common/section'
+
 export interface HomeProps {
-    className?: string;
-    id?: string;
+  className?: string
+  id?: string
 }
 
 /**
@@ -14,10 +13,10 @@ export interface HomeProps {
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const Home = ({ className }: HomeProps) => {
-    return (
-        <Section className={className}>
-            <NameAndTitle />
-            {/* <img src={WALLPAPER} className="fixed top-0 left-0 w-screen h-screen object-cover z-0"></img> */}
-        </Section>
-    );
-};
+  return (
+    <Section className={className}>
+      <NameAndTitle />
+      {/* <img src={WALLPAPER} className="fixed top-0 left-0 w-screen h-screen object-cover z-0"></img> */}
+    </Section>
+  )
+}
